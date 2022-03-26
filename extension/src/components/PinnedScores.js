ab6e6fb3efa3233f59d5be65c2fa809473fb9b93
Nemesis <keziah.moselle@gmail.com>
2022-03-26 14:47:39 +0100

    feat: remove auto refresh for pinned scores

--- extension/src/components/PinnedScores.js
+++ extension/src/components/PinnedScores.js
@@ -1,9 +1,6 @@
 import useSWR from 'swr'
-import { refreshIntervalPinnedScores } from '../constants'
-import usePercent from '../hooks/usePercent'
 import { fetcher } from '../libs/axios'
 import Loading from './Loading'
-import ProgressBar from './ProgressBar'
 import Score from './Score'
 import UpdatedAt from './UpdatedAt'
 import { useEffect, useState } from 'react'
@@ -11,14 +8,12 @@ import { useEffect, useState } from 'react'
 export default function BestPerformance({ config }) {
   const [updatedAt, setUpdatedAt] = useState(new Date())
   const qs = new URLSearchParams(config)
-  const { data: pinnedScores, isValidating, mutate } = useSWR(
+  const { data: pinnedScores, isValidating } = useSWR(
     `v1/pinned_scores?${qs}`,
     fetcher, {
-      refreshInterval: refreshIntervalPinnedScores,
       revalidateOnFocus: false
     }
   )
-  const { percent } = usePercent(60 * 60, mutate)
 
   useEffect(() => {
     setUpdatedAt(new Date())
@@ -26,8 +21,6 @@ export default function BestPerformance({ config }) {
 
   return (
     <div className="flex flex-col gap-y-1">
-      <ProgressBar percent={percent} />
-
       {isValidating && (
         <Loading />
       )}
--- extension/src/constants.js
+++ extension/src/constants.js
@@ -1,3 +1,2 @@
-export const refreshIntervalPinnedScores = 1000 * 60 * 60
 export const refreshIntervalBestPerformance = 1000 * 60 * 5
 export const refreshIntervalRecentScores = 1000 * 30
